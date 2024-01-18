869eac77a29e1f05e84c9209b3d3f332500c0a7c
TAHER_Home <ziaee.taher@gmail.com>
2024-01-18 10:28:07 +0330

    Car.py edited

--- S10Exr/car.py
+++ S10Exr/car.py
@@ -1,11 +1,24 @@
 class Car:
-    def __init__(self)->None:
+    def __init__(self,brand:str,model:str)->None:
         self.brand=brand
         self.model=""
         self.year=""
 
-    def get_car(self)->None:
+    def get_car_specs(self)->None:
         print("-- Please enter car specifications --")
         self.brand=input("Brand: ")
         self.model=input("Model: ")
         self.year=input("Year: ")
+
+car=None
+def get_car()->Car:
+    global car
+    if not car:
+        car=Car()
+    return car
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
